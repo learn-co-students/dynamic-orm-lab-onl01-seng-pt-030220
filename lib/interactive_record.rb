875abef8218fc8b7383b93975febcf47f5439b95
MMcClure11 <mcclme02@luther.edu>
2020-04-27 14:51:56 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,13 +55,16 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute_hash)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = ?"
-    DB[:conn].execute(sql, attribute_hash.values)
-    # binding.pry
+    attribute_hash.map do |attribute, value|
+      sql = "SELECT * FROM #{self.table_name} WHERE #{attribute} = ?"
+      DB[:conn].execute(sql, "#{value}")[0]
+    end
+  end
+    # sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = ?"
+    # DB[:conn].execute(sql, attribute_hash.values)
+    
     # value = attribute_hash.values.first
     # formatted_value = value.class == Fixnum ? value : "'#{value}'"
     # sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     # DB[:conn].execute(sql)
-  end
-
 end
